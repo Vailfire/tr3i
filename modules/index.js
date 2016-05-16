97fa8b7f0b42f4c4edddc5fa700ed60965e2ec17
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2016-05-17 01:50:33 +0200

    fixed linter issues

--- modules/index.js
+++ modules/index.js
@@ -1,4 +1,4 @@
-import usb = from 'usb';
+import usb from 'usb';
 
 usb.setDebugLevel(4);
 
@@ -8,30 +8,30 @@ nxt.open();
 
 console.log(nxt);
 
-const interface = nxt.interfaces[0];
-interface.claim();
-console.log(interface);
+const nxtInterface = nxt.interfaces[0];
+nxtInterface.claim();
+console.log(nxtInterface);
 
-const outEndpoint = interface.endpoints[0];
-cpnst inEndpoint = interface.endpoints[1];
+const outEndpoint = nxtInterface.endpoints[0];
+const inEndpoint = nxtInterface.endpoints[1];
 
 // '[0x80,0x03,0x0B,0x02,0xF4,0x01]
-const startMotor = [0x00,0x04,0x02,0x64,0x07,0x00,0x00,0x20,0x168,0x00,0x00,0x00];
+const startMotor = [0x00, 0x04, 0x02, 0x64, 0x07, 0x00, 0x00, 0x20, 0x168, 0x00, 0x00, 0x00];
 
 console.log(startMotor);
 
-outEndpoint.transfer(new Buffer(startMotor), function (err) {
-	if(err) console.log(err);
+outEndpoint.transfer(new Buffer(startMotor), (err) => {
+	console.log(err);
 });
 
-inEndpoint.transfer(3, function(err, data){
-	console.log("received data, ", data);
+inEndpoint.transfer(3, (err, data) => {
+	console.log('received data', data);
 });
 
-usb.on('attach', function(device) {
-	console.log('attached: ',device);
+usb.on('attach', (device) => {
+	console.log('attached: ', device);
 });
 
-usb.on('detach', function(device) {
-	console.log('detached: ',device);
+usb.on('detach', (device) => {
+	console.log('detached: ', device);
 });
