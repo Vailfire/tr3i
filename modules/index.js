ef4540c0019e871b0a4ea9c7515c66ddf133c288
Calvin Reibenspieß <calvinreibenspiess+fitAG@gmail.com>
2016-05-20 14:53:30 +0200

    small experiments

--- modules/index.js
+++ modules/index.js
@@ -4,8 +4,9 @@ const startMotor = [0x00, 0x04, 0xFF, 0x64, 0x07, 0x00, 0x00, 0x20, 0x00, 0x00, 
 
 detectAttachedBricks.then((nxtBricks) => {
   console.log(nxtBricks);
-  openConnection(nxtBricks[0]).then(({ inEndpoint, outEndpoint }) => {
+  openConnection(nxtBricks[0]).then(({ device, inEndpoint, outEndpoint }) => {
     console.log(startMotor);
+    console.log(device, device.interface[0]);
     outEndpoint.transfer(new Buffer(startMotor), (err) => {
       console.log('executed', err);
     });
