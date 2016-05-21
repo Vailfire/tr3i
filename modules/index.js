4bc74bfd9ec92004d93b2a6eb35a1564da523564
Calvin Reibenspieß <calvinreibenspiess+fitAG@gmail.com>
2016-05-21 11:47:55 +0200

    small experiments

--- modules/index.js
+++ modules/index.js
@@ -9,20 +9,22 @@ const stopMotor = [0x00, 0x04, 0xFF, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0
 detectAttachedBricks.then((nxtBricks) => {
   console.log(nxtBricks);
   openConnection(nxtBricks[0]).then(({ inEndpoint, outEndpoint }) => {
-    console.log(startMotor);
-    outEndpoint.transfer(new Buffer(startMotor(MOTOR_PORT.B)), (err) => {
-      console.log('executed', err);
-    });
+    const delayAction = (port, delay) => {
+      setTimeout(() => {
+        outEndpoint.transfer(new Buffer(stopMotor), (err) => {
+          console.log('executed', err);
+        });
+        outEndpoint.transfer(new Buffer(startMotor(port)), (err) => {
+          console.log('executed', err);
+        });
+      }, delay);
+    };
+    delayAction(MOTOR_PORT.ALL, 0);
+    delayAction(MOTOR_PORT.A, 2000);
+    delayAction(MOTOR_PORT.B, 2000);
+    delayAction(MOTOR_PORT.C, 2000);
     inEndpoint.transfer(3, (err, data) => {
       console.log('received data', data);
     });
-    setTimeout(() => {
-      outEndpoint.transfer(new Buffer(stopMotor), (err) => {
-        console.log('executed', err);
-      });
-      outEndpoint.transfer(new Buffer(startMotor(MOTOR_PORT.C)), (err) => {
-        console.log('executed', err);
-      });
-    }, 1400);
   });
 });
