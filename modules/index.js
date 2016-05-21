1419934e3e01cf5376ac70468a340010872f7bc0
Calvin Reibenspieß <calvinreibenspiess+fitAG@gmail.com>
2016-05-21 11:32:35 +0200

    small experiments

--- modules/index.js
+++ modules/index.js
@@ -7,7 +7,7 @@ const stopMotor = [0x00, 0x04, 0xFF, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0
 detectAttachedBricks.then((nxtBricks) => {
   console.log(nxtBricks);
   openConnection(nxtBricks[0]).then(({ inEndpoint, outEndpoint }) => {
-    console.log(startMotor);
+    console.log(startMotor.splice(2, 2, MOTOR_PORT.B));
     outEndpoint.transfer(new Buffer(startMotor.splice(2, 2, MOTOR_PORT.B)), (err) => {
       console.log('executed', err);
     });
