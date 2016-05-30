4295a1d78834ab395240273c866c487c6a5765dc
Calvin Reibenspieß <calvinreibenspiess+fitAG@gmail.com>
2016-05-30 15:02:12 +0200

    small experiments

--- modules/transmission/transmit.js
+++ modules/transmission/transmit.js
@@ -2,7 +2,7 @@ import { getEndpoints } from 'usb';
 
 export const sendCommand = (device, command) => new Promise((resolve, reject) => {
   const { outEndpoint } = getEndpoints(device);
-
+  console.log(`Sending command: ${command}`);
   outEndpoint.transfer(new Buffer(command), (error, answer) => {
     if (error) {
       reject(error);
